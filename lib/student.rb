815903bb97f8a33a9ecd7277c45a67fb90119e79
Catherine Salazar <catcreates13@gmail.com>
2018-09-07 01:44:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
 end
 
   def self.create(name, grade)
-    student = self.new
+    student = self.new(name, grade)
     student.save
     # student
     # DB[:conn].execute(sql, self.name, self.grade)
