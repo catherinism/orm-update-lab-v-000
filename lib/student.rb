f9ba94d50bd4a08646792a2aa03ab0886f3a9c86
Catherine Salazar <catcreates13@gmail.com>
2018-09-07 02:08:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,11 +50,11 @@ end
   end
 
   def self.new_from_db(row)
-   self.new(
-   row[0],
-   row[1],
-   row[2]
-   )
+    new_student = Student.new(name, grade = nil, id = nil)
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.find_by_name
